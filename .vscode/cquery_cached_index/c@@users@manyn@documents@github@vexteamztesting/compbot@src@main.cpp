08cd805fa856814d1e47ea5cb7cfffd6e695072f
braxtonkotter <kotterb22@mynuames.org>
2021-10-12 16:00:04 -0600

    Basic 4 motor tank drive setup completed

--- .vscode/cquery_cached_index/c@@users@manyn@documents@github@vexteamztesting/compbot@src@main.cpp
+++ .vscode/cquery_cached_index/c@@users@manyn@documents@github@vexteamztesting/compbot@src@main.cpp
@@ -1,20 +1,15 @@
 #include "main.h"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
+//positive
+#define LEFT_MOTOR_FRONT 10
+#define RIGHT_MOTOR_BACK 1
+
+//negitive
+#define LEFT_MOTOR_BACK 9
+#define RIGHT_MOTOR_FRONT 2
+
+int leftInput;
+int rightInput;
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -23,10 +18,7 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -58,7 +50,9 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -74,19 +68,25 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	pros::Motor rb_mtr(RIGHT_MOTOR_BACK, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
+	pros::Motor rf_mtr(RIGHT_MOTOR_FRONT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+	pros::Motor lb_mtr(LEFT_MOTOR_BACK, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+	pros::Motor lf_mtr(LEFT_MOTOR_FRONT, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+	while(true){
+		rightInput = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+		leftInput = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
 
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+		std::cout << rightInput << std::endl;
+
+		rb_mtr = rightInput;
+		rf_mtr = rightInput;
+
+		lf_mtr = leftInput;
+		lb_mtr = leftInput;
+		pros::delay(10);
 	}
+
+
 }
