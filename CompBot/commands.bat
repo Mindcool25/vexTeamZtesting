1803c00581bb8c8771db5b0ef927289fc68c7759
braxtonkotter <kotterb22@mynuames.org>
2021-12-10 15:18:47 -0700

    testing commit in command line

---
+++ CompBot/commands.bat
@@ -0,0 +1,9 @@
+@echo off
+
+doskey quick = pros build quick
+doskey all = pros build all
+doskey upload = pros upload 
+doskey terminal = pros terminal
+doskey commit %1 = git commit -m %1
+doskey add = git add .
+doskey push = git push
