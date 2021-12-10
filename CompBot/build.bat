239093a5fb2cea3b437ae67984cd7e43868f9716
braxtonkotter <54378566+braxtonkotter@users.noreply.github.com>
2021-12-10 11:40:12 -0700

    Update build.bat

--- CompBot/build.bat
+++ CompBot/build.bat
@@ -6,6 +6,5 @@ pros upload
 doskey quick = pros build quick
 doskey all = pros build all
 doskey upload = pros upload
-doskey upquick = call :quickUpload 
+doskey upquick = call :quickUpload
 doskey terminal = pros terminal
-
