0da0ad84d423ba6c4cef49e1fbd10c1249120d0b
braxtonkotter <kotterb22@mynuames.org>
2021-12-10 15:29:16 -0700

    Update commands.bat

--- CompBot/commands.bat
+++ CompBot/commands.bat
@@ -4,6 +4,6 @@ doskey quick = pros build quick
 doskey all = pros build all
 doskey upload = pros upload 
 doskey terminal = pros terminal
-doskey commit %1 = git commit -m %1
+doskey commit = git commit
 doskey add = git add .
 doskey push = git push
